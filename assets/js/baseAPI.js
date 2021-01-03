cda17bc8556c1883e92a185ac33dd153676ae263
hiede-in-shadow <1039372261@qq.com>
2021-01-03 15:28:59 +0800

    提交

--- assets/js/baseAPI.js
+++ assets/js/baseAPI.js
@@ -17,7 +17,7 @@ $.ajaxPrefilter(function (options) {
     options.complete = function (res) {
       if (res.responseJSON.status === 1 && res.responseJSON.message === '身份认证失败！') {
         localStorage.removeItem('token')
-        location.href = '/login.html'
+        window.top.location.href = '/login.html'
       }
     }
   }
